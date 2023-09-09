0f24fbf276d67bcecd72b5911adfe5e8a09896a3
dinhhuunam <dinhhuunam.job@gmail.com>
2023-09-09 22:40:34 +0700

    delete Inheritance 1

--- OOP_project/src/Inheritance1/Runner.java
+++
@@ -1,9 +0,0 @@
-package Inheritance3;
-
-public class Runner {
-	public static void main(String[] args) {
-		Cylinder h1=new Cylinder(5,2);
-		System.out.println(h1.getArea());
-		System.out.println(h1.getVolume());
-	}
-}
